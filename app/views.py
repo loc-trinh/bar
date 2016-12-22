5cf210e9b3657b7021de6c5852aa864778fe365d
Loc Trinh <loctrinh@mit.edu>
2016-12-22 02:29:51 -0800

    clean up views, search, and added hover for searchbar

--- app/views.py
+++ app/views.py
@@ -1,21 +1,17 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
 from django.db.models import Q
 
 from models import Drink
 
 def index(request):
-	context = {}
-	
 	drinks = Drink.objects.all()
 	num_pages = (len(drinks)-1)/9 + 1
-	context['pagination'] = range(1, num_pages+1)
-	context['active_page'] = 1
-	context['max_page'] = num_pages
 
-	drinks = drinks[0:9]
+	context = {}
+	context['max_page'] = num_pages
+	context['active_page'] = 1
+	context['pagination'] = range(1, num_pages+1)
 	context['rows'] = [drinks[i:i + 3] for i in xrange(0, 9, 3)]
 	
 	return render(request, 'app/index.html', context)
@@ -26,18 +22,33 @@ def page(request, page_id):
 	if page_id == 1:
 		return redirect("/")
 
-	context = {}
-	
 	drinks = Drink.objects.all()
 	num_pages = (len(drinks)-1)/9 + 1
-	context['pagination'] = range(1, num_pages+1)
-	context['active_page'] = page_id
-	context['max_page'] = num_pages
 
+	context = {}
+	context['max_page'] = num_pages
+	context['active_page'] = page_id
+	context['pagination'] = range(1, num_pages+1)
+	
 	drinks = drinks[(page_id-1)*9:page_id*9]
 	context['rows'] = [drinks[i:i + 3] for i in xrange(0, 9, 3)]
 	
-	return render(request, 'app/index.html', context)
+	if page_id <= num_pages:
+		return render(request, 'app/index.html', context)
+	else:
+		raise Http404()
+
+def search(request):
+	query = request.GET["query"]
+	query_list = query.split(",")
+	drinks = Drink.objects.filter(reduce(lambda x, y: x & y, [Q(name__icontains=word) | Q(ingridients__icontains=word) | Q(instructions__icontains=word) for word in query_list]))
+	
+	context = {}
+	context['query'] = query
+	context['found'] = True if len(drinks) > 0 else False
+	context['rows'] = [drinks[i:i + 3] for i in xrange(0, len(drinks), 3)]
+	
+	return render(request, 'app/search.html', context)
 
 def favorite(request):
 	return render(request, 'app/favorite.html', {})
@@ -48,9 +59,4 @@ def available(request):
 def special(request):
 	return render(request, 'app/thespecial.html', {})
 
-def search(request):
-	query = request.GET["query"]
-	context = {}
-	drinks = Drink.objects.filter(reduce(lambda x, y: x & y, [Q(name__icontains=word) | Q(ingridients__icontains=word) | Q(instructions__icontains=word) for word in query.split(",")]))
-	context['rows'] = [drinks[i:i + 3] for i in xrange(0, len(drinks), 3)]
-	return render(request, 'app/search.html', context)
+
